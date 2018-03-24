bfed276986bb4b68b190901a04614c9a22e03dda
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-24 16:33:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,15 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Arry.new(9, " ")
   end
+  WIN_COMBINATIONS = [
+  [0, 1, 2], # top row  index =  0
+  [3, 4, 5], # middle row
+  [6, 7, 8], # bottom row
+  [0, 3, 6], # left columns
+  [1, 4, 7], # middle columns
+  [2, 5, 8], # right columns
+  [0, 4, 8], # Ltop Rbottom diagonals
+  [6, 4, 2] # Rtop Lbottom diagonal
+]
+
 end
