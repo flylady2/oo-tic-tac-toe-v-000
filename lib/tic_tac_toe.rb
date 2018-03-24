3abb92831cb4b251adf79dfe180a65826ce55106
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-24 16:41:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,9 @@ class TicTacToe
 def display_board
 puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n-----------\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n-----------\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
 end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
 
 end
