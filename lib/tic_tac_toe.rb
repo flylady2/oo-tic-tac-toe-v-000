8de7b9fb207bdd1445074a1e9d5feef20994bc1f
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-24 17:24:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,6 +33,17 @@ end
 def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 
 
 
