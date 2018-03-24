9d44f1b9a0f777cb600fb790dc7a60154a3cab57
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-24 16:37:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,5 +12,9 @@ class TicTacToe
   [0, 4, 8], # Ltop Rbottom diagonals
   [6, 4, 2] # Rtop Lbottom diagonal
 ]
+def display_board
+puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
+
 
 end
